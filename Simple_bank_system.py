3045a888daebc0e2fcf3ef0b4638a06656e6f761
Serguei Gorbounov <127092671+sergueigorbounov@users.noreply.github.com>
2025-01-13 10:16:32 +0100

    Update Simple_bank_system.py

--- Simple_bank_system.py
+++ Simple_bank_system.py
@@ -24,8 +24,4 @@ class Bank:
                 self.balance[account - 1] -= money
                 return True
         return False
-# Your Bank object will be instantiated and called as such:
-# obj = Bank(balance)
-# param_1 = obj.transfer(account1,account2,money)
-# param_2 = obj.deposit(account,money)
-# param_3 = obj.withdraw(account,money)
+        
